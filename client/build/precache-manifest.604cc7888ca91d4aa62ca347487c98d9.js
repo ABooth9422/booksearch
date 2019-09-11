6758847f87bffb5d5c1a1373e71b66d7dd91e8c2
Jester <ABooth9422@gmail.com>
2019-09-10 21:29:17 -0400

    fixed deployment

---
+++ client/build/precache-manifest.604cc7888ca91d4aa62ca347487c98d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9fc2a6242340362d33c9",
+    "url": "/static/js/main.7c6c3f08.chunk.js"
+  },
+  {
+    "revision": "29a86166396e4b960d81",
+    "url": "/static/js/2.71a58411.chunk.js"
+  },
+  {
+    "revision": "9fc2a6242340362d33c9",
+    "url": "/static/css/main.2c0959d7.chunk.css"
+  },
+  {
+    "revision": "2cba2def9e2211cf44139cf98f8f8e41",
+    "url": "/index.html"
+  }
+];
